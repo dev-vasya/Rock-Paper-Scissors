8a9830f7b4fdb1f573232f6199e9a780d00f4d95
vasya-dev <learn.vasya@gmail.com>
2024-12-26 18:43:26 +0200

    add: getComputerChoice() function

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// console.log(Math.floor((Math.random() * 3) + 1))
+
+
+function getComputerChoice() {
+    let randomNumber = Math.floor((Math.random() * 3) + 1);
+    let computerChoice;
+
+    if (randomNumber == 1) {
+        return computerChoice = "Rock"
+    } else if (randomNumber == 2) {
+        return computerChoice = "Paper"
+    } else {
+       return computerChoice = "Scissors"
+    }
+}
+
+
+
+
