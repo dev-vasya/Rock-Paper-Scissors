66b7df014774fbf5a023693ee50a2bd78bb30fe5
vasya-dev <learn.vasya@gmail.com>
2024-12-26 18:55:22 +0200

    add getHumanChoice() function

--- scripts.js
+++ scripts.js
@@ -1,19 +1,25 @@
-// console.log(Math.floor((Math.random() * 3) + 1))
 
-
+// function calculates random number in range from 1 to 3, and then assigns string with game-value 
 function getComputerChoice() {
+    // get random number in rage from 1 to 3 include
     let randomNumber = Math.floor((Math.random() * 3) + 1);
     let computerChoice;
 
+    //asigns game-value, for further comparison
     if (randomNumber == 1) {
-        return computerChoice = "Rock"
+        return computerChoice = "rock"
     } else if (randomNumber == 2) {
-        return computerChoice = "Paper"
+        return computerChoice = "paper"
     } else {
-       return computerChoice = "Scissors"
+       return computerChoice = "scissors"
     }
 }
 
+// function take user input from modal window and return game-value(power)
+function getHumanChoice(){
+    let humanChoice = prompt("Write your power");
+    return humanChoice.toLowerCase();
+}
 
 
 
